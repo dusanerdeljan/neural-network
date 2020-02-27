bc4a9ecfd1b66d2e3bb04177ed6c3d1ec3b0197a
dusanerdeljan <dusanerdeljan99@gmail.com>
2020-02-27 22:47:57 +0100

    Code refactoring.

--- NeuralNetwork/Main.cpp
+++ NeuralNetwork/Main.cpp
@@ -5,7 +5,8 @@ int main()
 {
 	// Example usage: input layer has 2 neurons, output layer has 1 neuron
 	NeuralNetwork nn(2, {	
-		Layer(2, 1, ActivationFunctions::Type::SIGMOID),			// Input
+		Layer(2, 2, ActivationFunctions::Type::SIGMOID),
+		Layer(2, 1, ActivationFunctions::Type::SIGMOID)
 	});
 
 	// Matrix indexing example
@@ -16,7 +17,10 @@ int main()
 	//std::cout << matrix[{2, 0}] << std::endl;
 
 	// Simple training
-	nn.SimpleTraining(10000, { {{1, 0}, 1},{ { 1, 1 }, 0},{ { 0, 1 }, 0},{ { 0, 0 }, 0} });
+	//nn.SimpleTraining(20000, { {{1, 0}, 1},{ { 1, 1 }, 0},{ { 0, 1 }, 1},{ { 0, 0 }, 0} });
+
+	// SGD training
+	nn.SGD(1000, 0.01, { { { 1, 0 }, 1 }, { { 1, 1 }, 0 }, { { 0, 1 }, 1 }, { { 0, 0 }, 0 } });
 
 	std::cin.get();
 	return 0;
